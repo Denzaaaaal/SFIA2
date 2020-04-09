6f8d8ae5f0657369d7cee72c3a8695182c0ddba5
Denzaaaaal <denzeldouglas@protonmail.com>
2020-04-09 15:42:03 +0000

    fixed installation

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # installing ansible
-sudo apt update
-sudo apt install software-properties-common
+sudo apt update -y
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 
