d6ffac58fa3c71bf661b00663b480812f23032e5
Denzaaaaal <denzeldouglas@protonmail.com>
2020-04-10 18:20:49 +0000

    updated ansible to download files to from git

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,6 +3,6 @@
 # installing ansible
 sudo apt update -y -qq > /dev/null
 sudo apt install software-properties-common -y -qq > /dev/null
-sudo apt-add-repository --yes --update ppa:ansible/ansible -y -qq > /dev/null
+sudo apt-add-repository --yes --update ppa:ansible/ansible -y 
 sudo apt install ansible -y -qq > /dev/null
 
