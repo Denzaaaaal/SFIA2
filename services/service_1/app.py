b0278d73bdfa71570d01a91d3cdf81d74c47b84a
Denzaaaaal <denzeldouglas@protonmail.com>
2020-05-10 18:43:32 +0000

    amdended to add Create functionality for CRUD

--- services/service_1/app.py
+++ services/service_1/app.py
@@ -12,14 +12,25 @@ app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
 
 @app.route("/", methods = ['GET','POST'])
 def home():
-    if request.method == 'POST':
-        combined_name = requests.get("http://service_4:5003/joined_names")
-        full_name = combined_name.text
-        cur=mysql.connection.cursor()
-        cur.execute('insert into name(full_name) values (%s)', [full_name])
+    if request.method == 'POST' and if "gen_name" in request.form:
+        # Creating name
+        generated_first_name = requests.get("http://service_4:5003/first_name")
+        generated_last_name = requests.get("http://service_4:5003/last_name")
+        first_name = generated_first_name.text
+        last_name = generated_last_name.text
+        cur = mysql.connection.cursor()
+        cur.execute('insert into names(first_name, last_name) values (%s, %s)', (first_name, last_name))
+        cur.execute('select * from names')
+        rows = cur.fetchall()
+        names = []
+
+        for row in rows: 
+            names.append(row)
+
         mysql.connection.commit()
-        cur.close()
-        return render_template("layout.html", full_name = full_name, title = "Name Generator")
-    return render_template("layout.html", full_name = "Hello!! Welcome to the site", title = "Name Generator")
+        cur.close() 
+        return render_template("layout.html", first_name = first_name, last_name = last_name, title = "Name Generator", names = names)
+    return render_template("instructions.html", title = "Name Generator")
+
 if __name__ == "__main__":
     app.run(port = 5000, host = "0.0.0.0", debug = True)
