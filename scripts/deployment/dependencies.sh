8d64788f577fcc26f8416c17b3cca19bd85ee1e1
Denzaaaaal <denzelbdouglas@gmail.com>
2020-10-04 15:36:24 +0100

    corrected dependencies

--- scripts/deployment/dependencies.sh
+++ scripts/deployment/dependencies.sh
@@ -5,6 +5,7 @@ sudo apt update -y -qq > /dev/null
 sudo apt install software-properties-common -y -qq > /dev/null
 sudo apt-add-repository --yes --update ppa:ansible/ansible -y 
 sudo apt install ansible -y -qq > /dev/null
+# installing testing packages
 sudo apt install python-pytest -y -qq > /dev/null
 sudo apt install coverage -y -qq > /dev/null
 sudo apt install python-pip -y -qq > /dev/null
